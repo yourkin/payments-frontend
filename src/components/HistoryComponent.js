ceb7c712cc235e5aa6519c46545aa19e30a601e1
Andrew Yourkin <andrew.yourkin@gmail.com>
2019-05-02 13:56:21 +0300

    Updated History table columns

--- src/components/HistoryComponent.js
+++ src/components/HistoryComponent.js
@@ -1,32 +1,65 @@
 import React from 'react';
 import { TRANSACTIONS } from '../shared/transactions';
 import BootstrapTable from 'react-bootstrap-table-next';
-import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import filterFactory, { textFilter, dateFilter } from 'react-bootstrap-table2-filter';
+
+//     "conversion_rate": 0.9,
+//     "received_amount": "9.00",
 
 function History(props) {
+    const dateFormatter = (date) => {
+        return date;
+    };
     const data = TRANSACTIONS;
     const columns = [{
-        dataField: 'id',
-        text: 'Transaction ID',
-        sort: true
+        dataField: 'transaction_date',
+        text: 'Date',
+        sort: true,
+        formatter: dateFormatter()
     }, {
-        dataField: 'sender_account',
+        dataField: 'sender_username',
         text: 'Sender',
         sort: true,
         filter: textFilter()
     }, {
-        dataField: 'receiver_account',
-        text: 'Receiver',
+        dataField: 'sender_currency',
+        text: 'Sender Currency',
         sort: true,
         filter: textFilter()
     }, {
         dataField: 'sent_amount',
-        text: 'Transfer Amount',
-        sort: true
+        text: 'Sent Amount',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'receiver_username',
+        text: 'Receiver',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'commission',
+        text: 'Commission',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'receiver_currency',
+        text: 'Receiver Currency',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'conversion_rate',
+        text: 'Conversion Rate',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'received_amount',
+        text: 'Received Amount',
+        sort: true,
+        filter: textFilter()
     }];
 
     return (
-        <div className="container">
+        <div className="m-5">
             <BootstrapTable keyField='id' data={ data } columns={ columns } bootstrap4 filter={ filterFactory() } />
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 
