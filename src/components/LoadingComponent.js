f51d5f22bacc14953bd0ce4bac292c8b17ba0e1c
Andrew Yourkin <andrew.yourkin@gmail.com>
2019-05-03 12:14:17 +0300

    Spinner position centered

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 export const Loading = () => {
     return (
-        <div className="col-12">
-            <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
-            <p>Loading . . .</p>
+        <div className="container">
+            <div className="row text-center">
+                <div className="col-12">
+                    <span className="fa fa-spinner fa-pulse fa-5x fa-fw text-primary"></span>
+                    <p>Loading . . .</p>
+                </div>
+            </div>
         </div>
     );
 };
