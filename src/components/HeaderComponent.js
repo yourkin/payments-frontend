0a84e1746aed18a47bbd5003d5610b0c0feb5b96
Andrew Yourkin <andrew.yourkin@gmail.com>
2019-05-05 18:37:24 +0300

    Styling

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -133,7 +133,7 @@ class Header extends Component {
                     </ModalBody>
                 </Modal>
 
-                <Navbar dark expand="md">
+                <Navbar expand="md" dark>
                     <div className="container">
 
                         <NavbarToggler onClick={this.toggleNav} />
