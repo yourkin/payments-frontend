a8ce944da98d0efc3db002f9c4d0e9cf31f98f0a
Andrew Yourkin <andrew.yourkin@gmail.com>
2019-05-09 18:34:48 +0300

    Fixed registration path, ugh

--- src/shared/urlPaths.js
+++ src/shared/urlPaths.js
@@ -2,5 +2,5 @@ export const transactions = 'api/transactions/';
 export const accounts = 'api/accounts/';
 export const currentUser = 'core/current_user/';
 export const tokenAuth = 'token-auth/';
-export const users = 'api/core/users/';
+export const users = 'core/users/';
 export const transfer = transactions;
