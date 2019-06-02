a2d200bcc1ccf914d9735c55482aca4491806d4d
Andrew Yourkin <andrew.yourkin@gmail.com>
2019-06-02 18:37:51 +0300

    using standard api user entrypoint

--- src/shared/urlPaths.js
+++ src/shared/urlPaths.js
@@ -2,5 +2,5 @@ export const transactions = 'api/transactions/';
 export const accounts = 'api/accounts/';
 export const currentUser = 'core/current_user/';
 export const tokenAuth = 'token-auth/';
-export const users = 'core/users/';
+export const users = 'api/users/';
 export const transfer = transactions;
